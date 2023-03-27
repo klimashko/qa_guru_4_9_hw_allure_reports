208d6f02f6692266fc9bb3e074b6f4b0c851b97f
evgenii klimashko <klimashko108@gmail.com>
2023-03-27 18:52:07 +0300

    Изменил allurdir в pytest.ini  на tests/allure-results. Добавил файл test_steps_with.py в нем ключевые шаги обернуты в разметку с помощью with allure.step('').

--- tests/test_selene.py
+++ tests/test_selene.py
@@ -9,11 +9,15 @@ from time import sleep
 
 def test_github():
     browser.open('https://github.com')
+
     s('.header-search-input').click()
     s('.header-search-input').send_keys('eroshenkoam/allure-example')
     s('.header-search-input').submit()
+
     s(by.link_text('eroshenkoam/allure-example')).click()
+
     s('#issues-tab').click()
+
     s(by.partial_text('#76')).should(be.visible)
 
 
---
+++ tests/test_steps_with.py
@@ -0,0 +1,33 @@
+import allure
+from selene import be
+from selene.support import by
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+from time import sleep
+
+
+
+
+def test_github():
+    with allure.step('Открываем главую страницу GitHub'):
+        browser.open('https://github.com')
+
+    with allure.step('Ищем репозиторий'):
+        s('.header-search-input').click()
+        s('.header-search-input').send_keys('eroshenkoam/allure-example')
+        s('.header-search-input').submit()
+
+    with allure.step('Открываем репозиторий'):
+        s(by.link_text('eroshenkoam/allure-example')).click()
+
+    with allure.step('Открываем Issues'):
+        s('#issues-tab').click()
+
+    with allure.step('Проверяем наличие Issue с номером 76'):
+        s(by.partial_text('#76')).should(be.visible)
+
+
+
+
+
+

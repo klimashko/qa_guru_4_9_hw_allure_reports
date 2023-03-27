43190461588d616b8fe1f665fc75feb749b42c3d
evgenii klimashko <klimashko108@gmail.com>
2023-03-27 16:06:20 +0300

    Добавил тест только на селене (без разметки шагов для репорта)

---
+++ tests/test_selene.py
@@ -0,0 +1,21 @@
+from selene import be
+from selene.support import by
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+from time import sleep
+
+
+
+
+def test_github():
+    browser.open('https://github.com')
+    s('.header-search-input').click()
+    s('.header-search-input').send_keys('eroshenkoam/allure-example')
+    s('.header-search-input').submit()
+    s(by.link_text('eroshenkoam/allure-example')).click()
+    s('#issues-tab').click()
+    s(by.partial_text('#76')).should(be.visible)
+
+
+
+
